e8ed159db6414cf8341cb1cd5ee7059e6117820f
Axellerosh <lubi.axellerosh@gmail.com>
2024-10-10 14:46:30 +0800

    removing cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,10 +1,10 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async () => {
+export const GET = async (request) => {
   try {
     await connectToDB();
-
+    console.log("Connected to database")
     const prompts = await Prompt.find({}).populate("creator");
     return new Response(JSON.stringify(prompts), { status: 200 });
     
